e40b4713924d1f82b25dd611abae264c50baa806
headlessNode <harriskhan047@outlook.com>
2023-07-01 19:27:53 +0500

    Complete exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    let result = '';
+    if(repeatTimes < 0){
+        return "ERROR";
+    }
+    else{
+        for(let i = 0 ; i<repeatTimes; i++){
+            result += string;
+        }
+    }
+      
+    return result;
 };
 
 // Do not edit below this line
