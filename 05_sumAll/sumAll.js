ee3ef789150ce9a5702f10ac1a4543261dc108b7
headlessNode <harriskhan047@outlook.com>
2023-07-02 17:43:19 +0500

    Complete exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,38 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    //check if any parameter is not a number
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        return "ERROR";
+    }
+    else{
+        
+        //check for if any parameter is -ve
+        if(Math.sign(a) == -1 || Math.sign(b) == -1){
+            return "ERROR";
+        }
+        
+        //check for which parameter is greater
+        if(a > b){
+            //code
+            let result = 0;
+            for(let i = 1; i<=a;i++){
+                result += i;
+            }
+
+            return result;
+        
+        }
+        
+        else{
+            //code
+            let result = 0;
+            for(let i = 1; i<=b;i++){
+                result += i;
+            }
+
+            return result;        
+        }
+    }
 
 };
 
