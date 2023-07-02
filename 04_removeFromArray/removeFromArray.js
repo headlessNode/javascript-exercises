ab382224332a9f558ff2f439e1dd9f3c4672e938
headlessNode <harriskhan047@outlook.com>
2023-07-02 12:50:04 +0500

    Complete exercise-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    return array.filter(function(x){
+        return !args.includes(x);
+    });
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
